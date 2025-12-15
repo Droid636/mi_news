94b3cdb5e18ed4b21674e99bd6faea2a23c62961
Droid636 <2022043@utsh.edu.mx>
2025-12-15 14:41:12 -0600

    app theme

---
+++ lib/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      // Aquí se definirán los colores y estilos globales
+      // Esperando instrucciones del usuario
+    );
+  }
+}
